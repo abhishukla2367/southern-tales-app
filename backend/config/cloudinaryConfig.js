5d11d81d00f57bad55cb902bdbeb233e30c016df
abhishukla2367-lgtm <abhi.shukla2367@gmail.com>
2026-03-11 18:28:41 +0530

    remove all files from main

--- backend/config/cloudinaryConfig.js
+++
@@ -1,7 +0,0 @@
-const cloudinary = require('cloudinary').v2;
-
-// The SDK automatically looks for 'CLOUDINARY_URL' in your .env
-// You don't need to manually pass the key/secret here!
-cloudinary.config(); 
-
-module.exports = cloudinary;
